2d0d7d587701d6f947c29aa629d1f7bef7cbef55
cbennett <cbennett@conleycc.com>
2026-02-13 08:27:55 -0600

    revert back from email form

--- api/sendEmail/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        status: 200,
-        body: JSON.stringify({
-            received: req.body || null,
-            message: "Function executed"
-        })
-    };
-};
